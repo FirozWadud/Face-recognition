15f64b2002bb2e46666de7c6beea0d73121b24f0
FirozWadud <mdfirozwadud.adm@gmail.com>
2023-03-23 22:43:32 +0600

    first commit

--- encode_generator_mulit images.py
+++ encode_generator_mulit images.py
@@ -6,11 +6,11 @@ import pickle
 known_faces = {}
 
 # Traverse through each directory named after unique IDs within "Images" directory
-for person_dir in os.listdir("Images_multi"):
+for person_dir in os.listdir("Images_multi1"):
     person_id = person_dir  # Use the directory name as the person ID
     person_encodings = []
-    for img_file in os.listdir(os.path.join("Images_multi", person_dir)):
-        img_path = os.path.join("Images_multi", person_dir, img_file)
+    for img_file in os.listdir(os.path.join("Images_multi1", person_dir)):
+        img_path = os.path.join("Images_multi1", person_dir, img_file)
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         face_encodings = face_recognition.face_encodings(img)
@@ -20,5 +20,5 @@ for person_dir in os.listdir("Images_multi"):
         known_faces[person_id] = person_encodings
 
 # Save the encodings as a pickle file
-with open("known_faces.pkl", "wb") as f:
+with open("known_faces1.pkl", "wb") as f:
     pickle.dump(known_faces, f)
--- ip cam crop.py
+++ ip cam crop.py
@@ -1,4 +1,3 @@
-import csv
 import threading
 import queue
 import pickle
@@ -8,10 +7,8 @@ import cvzone
 import numpy as np
 import time
 
-filename = 'Data/attendance.csv'
-
 # Create a buffer to hold frames
-buffer_size = 10
+buffer_size = 5
 frame_buffer = queue.Queue(maxsize=buffer_size)
 
 # # Load known encodings
@@ -20,9 +17,8 @@ frame_buffer = queue.Queue(maxsize=buffer_size)
 #
 # encodeListKnown, studentIds = encodeListKnownWithIds
 #
-
 # Load known encodings
-with open('known_faces1.pkl', 'rb') as f:
+with open('known_faces.pkl', 'rb') as f:
     known_faces = pickle.load(f)
 
 # Extract the encodings and IDs from the dictionary
@@ -36,76 +32,6 @@ for person_id, face_encodings in known_faces.items():
 # Initialize FPS timer
 start_time = time.time()
 frame_count = 0
-
-
-
-def fetch_info(id):
-    # simulate fetching data that takes some time
-    global studentInfo, imgStudent
-
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        header = next(reader)  # get the header row
-        rows = list(reader)  # get the remaining rows
-        print(id)
-        for row in rows:
-            if row[0] == id:
-                # increase attendance by 1
-                attendance = int(row[4]) + 1
-                row[4] = str(attendance)
-                # update the row in the file
-                with open(filename, 'w') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(header)
-                    writer.writerows(rows)
-                return {
-                    'id': row[0],
-                    'name': row[1],
-                    'department': row[2],
-                    'starting_year': row[3],
-                    'total_attendance': str(attendance),
-                    'date': row[5],
-                    'time': row[6]
-                }
-        return None
-
-def show_info(info,faceDis):
-    global studentInfo, imgStudent
-
-    student_info = info
-
-    if student_info:
-        print(f"Student ID: {student_info['id']}")
-        print(f"Name: {student_info['name']}")
-        print(f"Department: {student_info['department']}")
-        print(f"Starting Year: {student_info['starting_year']}")
-        print(f"Total Attendance: {student_info['total_attendance']}")
-        print(f"Date: {student_info['date']}")
-        print(f"Time: {student_info['time']}")
-        print(faceDis)
-    else:
-        print(f"No student found with that ID")
-
-def update_time(id):
-    # Get current date and time
-    current_date = time.strftime('%Y-%m-%d')
-    current_time = time.strftime('%H:%M:%S')
-
-    with open(filename, 'r') as file:
-        reader = csv.reader(file)
-        header = next(reader)  # get the header row
-        rows = list(reader)  # get the remaining rows
-        #print(id)
-        for row in rows:
-            #print(row[0])
-            if row[0] == id:
-                row[7] = current_date
-                row[8] = current_time
-                # update the row in the file
-                with open(filename, 'w') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(header)
-                    writer.writerows(rows)
 
 
 # Function to read frames from the camera and put them in the buffer
@@ -130,9 +56,6 @@ def read_frames():
 # Function to process frames from the buffer
 def process_frames():
     global start_time, frame_count
-
-    recognized_faces = set()
-    entry_time = set()
     while True:
         if not frame_buffer.empty():
             # Get frame from buffer
@@ -140,7 +63,19 @@ def process_frames():
 
             # Resize and convert to RGB
             #imgS = frame
-            imgS = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+
+            height, width = frame.shape[:2]
+            yy1 = int(height/5)
+            yy2 = yy1+500
+            xx1 = int(width/3)
+            xx2 = xx1+600
+
+
+            # Crop image
+            imgS = frame[yy1:yy2, xx1:xx2]
+            frame = cv2.rectangle(frame, (xx1, yy1), (xx2, yy2), (0, 255, 0), 2)
+
+            imgS = cv2.resize(imgS, (0, 0), None, 0.5, 0.5)
             imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
             # Detect faces and encode
@@ -151,7 +86,12 @@ def process_frames():
 
             # Check for matches and draw boxes
             for faceLoc in faceCurFrame:
-                y1, x2, y2, x1 = [i * 4 for i in faceLoc]
+                y1, x2, y2, x1 = [i * 2for i in faceLoc]
+                y1 = y1+yy1
+                y2 = y2+yy1
+                x1 = x1+xx1
+                x2 = x2+xx1
+
                 bbox = x1, y1, x2 - x1, y2 - y1
                 frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 faceEncodings = face_recognition.face_encodings(imgS, [faceLoc])
@@ -163,15 +103,6 @@ def process_frames():
                         matchIndex = np.argmin(faceDis)
                         id = studentIds[matchIndex]
                         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-                        update_time(id)
-                        if cv2.waitKey(1) == ord('r'):
-                            recognized_faces.clear()
-                        if id not in recognized_faces:
-                            recognized_faces.add(id)
-                            #entry_time =
-                            show_info(fetch_info(id), faceDis)
-
                     else:
                         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
 
@@ -190,6 +121,7 @@ def process_frames():
                 break
 
     cv2.destroyAllWindows()
+
 
 # Create and start threads
 read_thread = threading.Thread(target=read_frames)
